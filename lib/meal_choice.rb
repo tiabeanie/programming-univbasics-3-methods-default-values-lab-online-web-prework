3dec7d2884cfb3948aa52b4dac824e8216b3f1af
Sydney Hunter <sydney.jane.hunter@gmail.com>
2019-10-27 20:53:10 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,8 +2,8 @@
 def meal_choice(veg1, veg2, protien='meat')
   puts "What a nutritious meal!"
   meal = "A plate of #{protien} with #{veg1} and #{veg2}."
+  puts meal
 end
 
-meal_choice(broccoli, macaroni, tofu)
 
 
